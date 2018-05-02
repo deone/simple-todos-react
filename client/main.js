8b979b27f527105f62b01d311638ac071136d119
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-05-02 15:14:57 +0000

    Fix bug to ensure list displays on browser.

--- client/main.js
+++ client/main.js
@@ -5,5 +5,5 @@ import { render } from 'react-dom';
 import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
-  render('<App />', document.getElementById('render-target'));
+  render(<App />, document.getElementById('render-target'));
 });
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -7,8 +7,8 @@ export default class App extends Component {
   getTasks() {
     return [
       { _id: 1, text: 'This is task 1' },
-      { _id: 1, text: 'This is task 2' },
-      { _id: 1, text: 'This is task 3' },
+      { _id: 2, text: 'This is task 2' },
+      { _id: 3, text: 'This is task 3' },
     ];
   }
 
