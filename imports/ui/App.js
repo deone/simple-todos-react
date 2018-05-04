c87abd2f859456751efe9de5bf5f746ba1dd75b9
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-05-04 13:17:39 +0000

    Update renderTasks to pass in showPrivateButton.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -42,7 +42,16 @@ class App extends Component {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
     return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
     ));
   }
 
